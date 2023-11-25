e1b208c08d4a2067517def8f396073288b3156fb
Ri4Chaard <yarik271202@gmail.com>
2023-11-25 20:06:22 +0200

    Public and logged in routes added

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { routes } from "../router";
+import React, { useContext } from "react";
+import { publicRoutes, loginRoutes } from "../router";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthContext } from "./context";
 
 export const AppRouter = () => {
+    const { login } = useContext(AuthContext);
     return (
         <Routes>
-            {routes.map((route) => (
-                <Route
-                    key={route.path}
-                    path={route.path}
-                    element={route.element}
-                />
-            ))}
+            {login
+                ? loginRoutes.map((route) => (
+                      <Route
+                          key={route.path}
+                          path={route.path}
+                          element={route.element}
+                      />
+                  ))
+                : publicRoutes.map((route) => (
+                      <Route
+                          key={route.path}
+                          path={route.path}
+                          element={route.element}
+                      />
+                  ))}
             <Route path="*" element={<Navigate to="/menu" replace />} />
         </Routes>
     );
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,13 @@ import { About } from "../pages/About";
 import { Login } from "../pages/Login";
 import { Menu } from "../pages/Menu";
 
-export const routes = [
+export const publicRoutes = [
     { path: "/menu", element: <Menu /> },
     { path: "/about", element: <About /> },
     { path: "/login", element: <Login /> },
+];
+
+export const loginRoutes = [
+    { path: "/menu", element: <Menu /> },
+    { path: "/about", element: <About /> },
 ];
